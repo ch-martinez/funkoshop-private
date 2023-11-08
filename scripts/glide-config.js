2d8679946b7631a15daf357aa082475f79d6236f
Christian Martinez <chmartinez.cba@gmail.com>
2023-11-08 20:19:16 -0300

    [RESPONSIVE] Cards y Glide -mobile

--- scripts/glide-config.js
+++ scripts/glide-config.js
@@ -4,6 +4,10 @@ new Glide('.glide',
         type: 'carousel',
         startAt: 1,
         perView: 4,
+        peek: {
+            before: 50,
+            after: 50
+        },
         breakpoints: {
             991: {
                 perView: 2

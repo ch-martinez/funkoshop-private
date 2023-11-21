9af0e1f8b5dfd53321002d276348d7d90830daff
Christian Martinez <chmartinez.cba@gmail.com>
2023-11-21 16:53:29 -0300

    [RESPONSIVE] Estilos submenu en menu -mobile

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector('.navbar-mobile__menu-btn')
 const navbar = document.querySelector('.navbar__menu')
+const submenuBtn = document.querySelector('.with-icon')
+const submenu = document.querySelector('.submenu')
 
 const cantidaditems = () => {
     const items = document.querySelectorAll('.navbar__cart-count')
@@ -11,7 +13,14 @@ const cantidaditems = () => {
 }
 cantidaditems()
 
+/* Despliega menu principal */
 menuBtn.addEventListener('click', () => {
     navbar.classList.toggle('navbar__menu--active')
     menuBtn.classList.toggle('navbar-mobile__menu-btn--active')
+})
+
+/* Despliega submenu */
+submenuBtn.addEventListener('click', () => {
+    console.log("hola")
+    submenu.classList.toggle('--submenu-show')
 })

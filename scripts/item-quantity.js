08d403c45b04a9e0e4734ddab49b8f4a7678d740
Christian Martinez <chmartinez.cba@gmail.com>
2023-11-30 13:00:05 -0300

    [FIX]

--- scripts/item-quantity.js
+++ scripts/item-quantity.js
@@ -1,8 +1,8 @@
-const amount__input = document.querySelector('#amount__input')
-const btn__add = document.querySelector('#btn__add')
-const btn__substract = document.querySelector('#btn__substract')
+/* const amount__input = document.querySelectorAll('.amount__input')
+const btn__add = document.querySelectorAll('.btn--add')
+const btn__substract = document.querySelectorAll('.btn--substract')
 
 btn__add.addEventListener('click', () => amount__input.value = Number(amount__input.value) + 1)
 btn__substract.addEventListener('click', () => {
     if (amount__input.value > 1)  amount__input.value = Number(amount__input.value) - 1
-})
+}) */
